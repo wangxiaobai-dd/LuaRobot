7ad24586095d48a916f79a67fb49ab90a39f1a50
wangxiaobai-dd <leoeee@icloud.com>
2023-11-29 23:29:40 +0800

    update

--- lua_scripts/server.lua
+++ lua_scripts/server.lua
@@ -6,13 +6,43 @@ local sessionid_coroutine = {}
 local core_new_service = core.new_service
 local server = core
 
+
+local co_num = 0
+local co_pool = setmetatable({}, { __mode = "kv" })
+
+local function invoke(co, fn, ...)
+    co_num = co_num + 1
+    fn(...)
+    co_num = co_num - 1
+    co_pool[#co_pool + 1] = co
+end
+
+local function routine(fn, ...)
+    local co = co_running()
+    invoke(co, fn, ...)
+    while true do
+        invoke(co, co_yield())
+    end
+end
+
+local function coresume(co, ...)
+    local ok, err = co_resume(co, ...)
+    if not ok then
+        err = traceback(co, tostring(err))
+        co_close(co)
+        error(err)
+    end
+    return ok, err
+end
+
+-- 异步接口
 function server.async(fn, ...)
-    local co = tremove(co_pool) or co_create(routine)
+    local co = table.remove(co_pool) or coroutine.create(routine)
     coresume(co, fn, ...)
     return co
 end
 
---- 给协程映射id
+-- 映射协程id
 function server.make_session()
     uid = uid + 1
     if nil ~= sessionid_coroutine[uid] then
@@ -23,6 +53,7 @@ function server.make_session()
     return uid
 end
 
+-- 创建新服务
 function server.new_service(option)
     local sessionid = server.make_session()
     core_new_service(sessionid, option)
