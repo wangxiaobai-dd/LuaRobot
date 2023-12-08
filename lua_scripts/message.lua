047b7595c94a7c37195ea577ba98be1f64eae499
wangxiaobai-dd <leoeee@icloud.com>
2023-12-08 11:48:07 +0800

    update

---
+++ lua_scripts/message.lua
@@ -0,0 +1,18 @@
+
+local core = require("core")
+local server = core
+
+------------------------------- 消息开始 -------------------------------
+
+-- 消息发送
+function server.send()
+    
+end
+
+-- 消息转发
+function server.dispatch()
+    
+end
+
+
+------------------------------- 消息结束 -------------------------------
--- lua_scripts/server.lua
+++ lua_scripts/server.lua
@@ -1,22 +1,9 @@
 require("service")
+require("message")
 
 local core = require("core")
 local server = core
 
-------------------------------- 消息开始 -------------------------------
-
--- 消息发送
-function server.send()
-    
-end
-
--- 消息转发
-function server.dispatch()
-    
-end
-
-
-------------------------------- 消息结束 -------------------------------
 
 
 print(package.path)
