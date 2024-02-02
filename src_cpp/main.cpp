c2fbc83f17acc6584f787ea3568ec67248a2e68b
wangxiaobai-dd <leoeee@icloud.com>
2024-02-02 18:39:13 +0800

    sol

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <thread>
 
+#include "sol.hpp"
+
 static void usage()
 {
     std::cout << "ÊäÈë²ÎÊý: xxx.lua, ./main example.lua" << std::endl;
@@ -32,34 +34,47 @@ int main(int argc, char const* argv[])
         return -1;
     }
 
-    lua_State* L = luaL_newstate();
-    luaL_openlibs(L);
-
-    int err = luaL_loadfile(L, script.data());
-    if(err != LUA_OK)
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    auto ret = lua.script_file(script.data());
+    if(!ret.valid())
     {
         report("load lua fail");
         return -1;
     }
+    /*
+        lua_State* L = luaL_newstate();
+        luaL_openlibs(L);
 
-    err = lua_pcall(L, 0, 1, 0);
-    if(err != LUA_OK)
+        int err = luaL_loadfile(L, script.data());
+        if(err != LUA_OK)
+        {
+            report("load lua fail");
+            return -1;
+        }
+         err = lua_pcall(L, 0, 1, 0);
+          if(err != LUA_OK)
     {
         report("exec lua fail");
         return -1;
     }
-
+    
     if(lua_type(L, -1) != LUA_TTABLE)
     {
         report("need load config table");
         return -1;
     }
+    */
+
+   
+   
+
     uint32_t threads = getLuaField<uint32_t>(L, -1, "threads");
     std::cout << "threads:" << threads << std::endl;
 
     // package path
     auto pathTable = getLuaField<LuaVector>(L, -1, "path_table");
-    
+
     std::string path = "package.path = '";
     for(const auto& p : pathTable.strVec)
     {
@@ -69,11 +84,11 @@ int main(int argc, char const* argv[])
     }
     path += "'";
     std::cout << path << std::endl;
-    
+
     auto server = std::make_shared<Server>(threads);
     server->envPath = path;
     server->luaDirs = pathTable.strVec;
-    
+
     // test
     auto option = std::make_unique<ServiceOption>();
     option->luaFile = "server.lua";
